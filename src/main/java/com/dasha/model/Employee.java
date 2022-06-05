e7599f982bd4cde1889d148554bf7543fda7219d
DGlobus <dasha.globchastaya.00@mail.ru>
2022-06-05 18:14:33 +1000

    add more annotations

--- src/main/java/com/dasha/model/Employee.java
+++ src/main/java/com/dasha/model/Employee.java
@@ -1,14 +1,11 @@
 package com.dasha.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Getter
-@Setter
+@Data
 @Builder
 public class Employee{
     private String firstName;
@@ -25,20 +22,5 @@ public class Employee{
                 + "\nDescription: " + description
                 + "\nCharacteristics: " + characteristics.stream().map(a->String.valueOf(a)).collect(Collectors.joining(", "))
                 + "\nPost: " + post.name + "\n\n";
-    }
-
-    @Override
-    public boolean equals(Object anotherEmployee){
-        Employee emp = (Employee) anotherEmployee;
-        if(emp != null){
-            if(firstName.equals(emp.getFirstName()))
-                if(lastName.equals(emp.getLastName()))
-                    if(description.equals(emp.getDescription()))
-                        if(characteristics.equals(characteristics))
-                            if(post.getId().equals(emp.getPost().getId()))
-                                return true;
-
-        }
-        return false;
     }
 }
--- src/main/java/com/dasha/model/Post.java
+++ src/main/java/com/dasha/model/Post.java
@@ -1,12 +1,13 @@
 package com.dasha.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
 
-@Setter
-@Getter
+@Data
 public class Post {
     UUID id;
     String name;
